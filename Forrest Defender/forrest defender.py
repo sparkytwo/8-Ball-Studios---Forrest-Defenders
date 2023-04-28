1319e81ac6ad9134d73c64e335b0b723ea2c7450
Wei Chen Beh <wei4.beh@live.uwe.ac.uk>
2023-04-28 12:52:54 +0100

    Arrows Implemented

--- Forrest Defender/forrest defender.py
+++ Forrest Defender/forrest defender.py
@@ -39,8 +39,15 @@ mana_drop = GameObject('Images/manadrop.png', (600, 300), (17, 20))
 # Initial Image
 menu_img = pygame.image.load('Images/Menu.png')
 menu_p_image = pygame.image.load('Images/PressedMenu.png')
+left_arrow_img = pygame.image.load('Images/Left Arrow.png')
+left_arrow_p_img = pygame.image.load('Images/Left Arrow Pressed.png')
+right_arrow_img = pygame.image.load('Images/Right Arrow.png')
+right_arrow_p_img = pygame.image.load('Images/Right Arrow Pressed.png')
+
 # Initialise Button
 menu = Button(1150, 0, menu_img, menu_p_image, 1)
+left_arrow = Button(20, 400, left_arrow_img, left_arrow_p_img, 1)
+right_arrow = Button(1230, 400, right_arrow_img, right_arrow_p_img, 1)
 
 # Initialise Text
 mana = font.render(str(mana_int), True, WHITE)
@@ -49,26 +56,38 @@ mana_incr_drop = font_small.render(str(mana_per_second), True, WHITE)
 mana_incr_drop_rect = pygame.Rect(630,300,100,100)
 # Update Function
 def update():
-    global mana_int
-    global mana
-    global alpha
-    # Increments Mana
-    mana_int += mana_per_second * dt
-    mana = font.render(str(int(mana_int)), True, WHITE)
-    # Checks Menu
-    if menu.clicked:
-        menu.c_img = menu.p_image
-    else:
-        menu.c_img = menu.d_image
-    # Mana Gain sprite
-    mana_drop.rect.y -= 250 * dt
-    mana_drop.image.set_alpha(alpha)
-    alpha += 300 * dt
-    mana_incr_drop_rect.y -= 250 * dt
-    if mana_drop.rect.y < 50:
-        mana_drop.rect.y = 300
-        mana_incr_drop_rect.y = 300
-        alpha = 0
+    if game_state == 1:
+        global mana_int
+        global mana
+        global alpha
+        # Increments Mana
+        mana_int += mana_per_second * dt
+        mana = font.render(str(int(mana_int)), True, WHITE)
+        # Checks Menu
+        if menu.clicked:
+            menu.c_img = menu.p_image
+        else:
+            menu.c_img = menu.d_image
+
+        if left_arrow.clicked:
+            left_arrow.c_img = left_arrow.p_image
+        else:
+            left_arrow.c_img = left_arrow.d_image
+
+        if right_arrow.clicked:
+            right_arrow.c_img = right_arrow.p_image
+        else:
+            right_arrow.c_img = right_arrow.d_image
+
+        # Mana Gain sprite
+        mana_drop.rect.y -= 250 * dt
+        mana_drop.image.set_alpha(alpha)
+        alpha += 300 * dt
+        mana_incr_drop_rect.y -= 250 * dt
+        if mana_drop.rect.y < 50:
+            mana_drop.rect.y = 300
+            mana_incr_drop_rect.y = 300
+            alpha = 0
     pygame.display.flip()
 
 
@@ -84,15 +103,19 @@ def render():
         screen.blit(menu.c_img, menu.rect)
         if menu_panel.visibility:
             screen.blit(menu_panel.image, menu_panel.rect)
+            screen.blit(left_arrow.c_img, left_arrow.rect)
+            screen.blit(right_arrow.c_img, right_arrow.rect)
 
 # Input Function
 def inputs():
-    if menu.click():
-        if menu_panel.visibility:
-            menu_panel.visibility = False
-        elif not menu_panel.visibility:
-            menu_panel.visibility = True
-
+    if game_state == 1:
+        if menu.click():
+            if menu_panel.visibility:
+                menu_panel.visibility = False
+            elif not menu_panel.visibility:
+                menu_panel.visibility = True
+        left_arrow.click()
+        right_arrow.click()
 
 
 running = True
